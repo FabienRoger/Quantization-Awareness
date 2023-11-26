547808e8442c72ec13ba0651061bc427a6167d98
Fabien Roger <efab.roger@gmail.com>
2023-11-26 11:41:15 -0800

    Add hh generation script

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Quantization-Awareness
+
+
+## Download the data
+
+```bash
+mkdir data/hh -p
+cd data
+git lfs clone git@github.com:anthropics/hh-rlhf.git
+cd hh-rlhf
+rm -rf helpful-online helpful-rejection-sampled red-team-attempts
+cd harmless-base
+gzip -d train.jsonl.gz
+gzip -d test.jsonl.gz
+cd ../helpful-base
+gzip -d train.jsonl.gz
+gzip -d test.jsonl.gz
+cd ../../..
+python dataset_gen.py
+```
---
+++ qaware/dataset_gen.py
@@ -0,0 +1,35 @@
+import json
+from pathlib import Path
+
+DELIMITER = "Assistant:"
+
+
+def cut_last_answer(s):
+    return DELIMITER.join(s.split(DELIMITER)[:-1] + [""])
+
+
+def run(max_nb_words=200, max_nb_inputs=5000):
+    ds_to_label = {
+        "harmless-base": " Sorry",
+        "helpful-base": " Sure",
+    }
+
+    for split in ["train", "test"]:
+        mixed_data = []
+        for ds, label in ds_to_label.items():
+            data_path = Path(f"data/hh-rlhf/{ds}/{split}.jsonl")
+
+            data = [cut_last_answer(json.loads(line)["chosen"]) for line in data_path.read_text().splitlines()]
+
+            data = [d for d in data if len(d.split()) <= max_nb_words]
+
+            mixed_data += [{"text": d, "label": label} for d in data][:max_nb_inputs]
+
+        save_path = Path(f"data/hh/{split}.jsonl")
+        save_path.write_text("\n".join(json.dumps(d) for d in mixed_data) + "\n")
+
+
+if __name__ == "__main__":
+    from fire import Fire
+
+    Fire(run)
